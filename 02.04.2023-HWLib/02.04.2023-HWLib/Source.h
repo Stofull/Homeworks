af47a0b4a702dda0696227eb2516ea9dbffb519c
Bayra_ry03@itstep.edu.az <Bayra_ry03@itstep.edu.az>
2023-04-12 06:15:47 -0700

    Hw

---
+++ 02.04.2023-HWLib/02.04.2023-HWLib/Source.h
@@ -0,0 +1,10 @@
+#ifndef myLab
+#define myLab
+
+const int LengthChar(char* str);
+void edit(char* str, char* old_w, char* new_w, int length);
+void point(char* str, int length);
+void letterCount(char* str, int* ar, int length);
+int findNum(char* str, int length, int count);
+void lowerCase(char* str, int length);
+#endif // Test my lab
