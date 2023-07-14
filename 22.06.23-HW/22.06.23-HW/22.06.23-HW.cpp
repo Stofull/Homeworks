08dc00cfba03f6ebe4e1a0c77bd22a0107a86f77
Bayra_ry03@itstep.edu.az <Bayra_ry03@itstep.edu.az>
2023-07-14 19:10:10 +0400

    HWQueue

--- 22.06.23-HW/22.06.23-HW/22.06.23-HW.cpp
+++ 22.06.23-HW/22.06.23-HW/22.06.23-HW.cpp
@@ -21,6 +21,12 @@ public:
 	{
 		this->capacity = list.size();
 		this->arr = new T[capacity]{};
+		size = capacity;
+		uint16_t index = 0;
+		for (const T& value : list)
+		{
+			this->arr[index++] = value;
+		}
 	}
 	~Queue()
 	{
@@ -37,23 +43,23 @@ public:
 		{
 			this->capacity *= 2;
 			T* newArr = new T[this->capacity]{};
-			for (size_t i = 0; i < this->capacity; i++)
+			for (size_t i = 0; i < this->size; i++)
 			{
 				newArr[i] = this->arr[i];
 			}
+			newArr[this->size] = value;
 			delete[] this->arr;
 			this->arr = newArr;
-			this->arr[this->size] = value;
-			size++;
+			this->size++;
 		}
 		else if (this->size == this->capacity && this->isExpandable == false)
 		{
-			cout << "Stack is full " << endl;
+			cout << "Queue is full " << endl;
 		}
 	}
 	void pop()
 	{
-		if (this->size == 0) cout << "Stack is empty " << endl;
+		if (this->size == 0) cout << "Queue is empty " << endl;
 		else
 		{
 			this->arr[this->size - 1] = {};
@@ -64,7 +70,121 @@ public:
 	{
 		if (this->size == 0)
 		{
-			cout << "Stack is empty " << endl;
+			cout << "Queue is empty " << endl;
+			return T();
+		}
+		else
+		{
+			return this->arr[0];
+		}
+	}
+	friend ostream& operator<<(ostream& out, const Queue<T>& Queue)
+	{
+		for (int i = 0; i < Queue.size; i++)
+		{
+			out << Queue.arr[i] << " ";
+		}
+		out << endl;
+		return out;
+	}
+};
+
+template <typename T>
+struct DeQueue
+{
+private:
+	uint16_t size{};
+	uint16_t capacity{};
+	T* arr{};
+	bool isExpandable{};
+public:
+	DeQueue(bool isExpandable, uint16_t capacity)
+	{
+		this->capacity = capacity;
+		this->arr = new T[capacity]{};
+		this->isExpandable = isExpandable;
+	}
+	DeQueue(initializer_list<T> list)
+	{
+		this->capacity = list.size();
+		this->arr = new T[capacity]{};
+		size = capacity;
+		uint16_t index = 0;
+		for (const T& value : list)
+		{
+			this->arr[index++] = value;
+		}
+	}
+	~DeQueue()
+	{
+		delete[] arr;
+	}
+	void pushBack(T value)
+	{
+		if (this->size < this->capacity) {
+			this->arr[this->size] = value;
+			this->size++;
+		}
+		else if (this->size == this->capacity && this->isExpandable == true) {
+			this->capacity *= 2;
+			T* newArr = new T[this->capacity]{};
+			for (size_t i = 0; i < this->size; i++) {
+				newArr[i] = this->arr[i];
+			}
+			newArr[this->size] = value;
+			delete[] this->arr;
+			this->arr = newArr;
+			this->size++;
+		}
+		else if (this->size == this->capacity && this->isExpandable == false) cout << "DeQueue is full " << endl;
+	}
+	void pushFront(T value)
+	{
+		if (this->size < this->capacity)
+		{
+			for (size_t i = this->size; i > 0; i--) {
+				this->arr[i] = this->arr[i - 1];
+			}
+			this->arr[0] = value;
+			this->size++;
+		}
+		else if (this->size == this->capacity && this->isExpandable == true)
+		{
+			this->capacity *= 2;
+			T* newArr = new T[this->capacity]{};
+			newArr[0] = value;
+			for (size_t i = 0; i < this->size; i++)
+			{
+				newArr[i + 1] = this->arr[i];
+			}
+			delete[] this->arr;
+			this->arr = newArr;
+			this->size++;
+		}
+		else if (this->size == this->capacity && this->isExpandable == false) cout << "DeQueue is full " << endl;
+	}
+	void popBack()
+	{
+		if (this->size == 0) cout << "DeQueue is empty " << endl;
+		else {
+			this->size--;
+		}
+	}
+	void popFront()
+	{
+		if (this->size == 0) cout << "DeQueue is empty " << endl;
+		else {
+			for (size_t i = 0; i < this->size - 1; i++) {
+				this->arr[i] = this->arr[i + 1];
+			}
+			this->size--;
+		}
+	}
+	T peek()
+	{
+		if (this->size == 0)
+		{
+			cout << "DeQueue is empty " << endl;
 			return T();
 		}
 		else
@@ -72,115 +192,29 @@ public:
 			return this->arr[this->size - 1];
 		}
 	}
-	friend ostream& operator<<(ostream& out, const Queue<T>& queue)
+	friend ostream& operator<<(ostream& out, const DeQueue<T>& deQueue)
 	{
-		for (int i = 0; i < queue.size; i++)
+		for (int i = 0; i < deQueue.size; i++)
 		{
-			out << queue.arr[i] << " ";
+			out << deQueue.arr[i] << " ";
 		}
 		out << endl;
 		return out;
 	}
 };
-//template <typename T>
-//struct deQueue
-//{
-//private:
-//	uint16_t size{};
-//	uint16_t capacity{};
-//	T* arr{};
-//	bool isExpandable{};
-//public:
-//	deQueue(bool isExpandable, uint16_t capacity)
-//	{
-//		this->capacity = capacity;
-//		this->arr = new T[capacity]{};
-//		this->isExpandable = isExpandable;
-//	}
-//	deQueue(initializer_list<T> list)
-//	{
-//		this->capacity = list.size();
-//		this->arr = new T[capacity]{};
-//	}
-//	~deQueue()
-//	{
-//		delete[] arr;
-//	}
-//	void pushBack(T value)
-//	{
-//		if (this->size < this->capacity)
-//		{
-//			this->arr[this->size] = value;
-//			this->size++;
-//		}
-//		else if (this->size == this->capacity && this->isExpandable == true)
-//		{
-//			this->capacity *= 2;
-//			T* newArr = new T[this->capacity]{};
-//			for (size_t i = 0; i < this->capacity; i++)
-//			{
-//				newArr[i] = this->arr[i];
-//			}
-//			delete[] this->arr;
-//			this->arr = newArr;
-//			this->arr[this->size] = value;
-//			size++;
-//		}
-//		else if (this->size == this->capacity && this->isExpandable == false) cout << "Stack is full " << endl;
-//	}
-//	void pushFront(T value)
-//	{
-//		if (this->size < this->capacity)
-//		{
-//			this->arr[this->size] = value;
-//			this->size++;
-//		}
-//		else if (this->size == this->capacity && this->isExpandable == true)
-//		{
-//			this->capacity *= 2;
-//			T* newArr = new T[this->capacity]{};
-//			for (size_t i = 0; i < this->capacity; i++)
-//			{
-//				newArr[i] = this->arr[i];
-//			}
-//			delete[] this->arr;
-//			this->arr = newArr;
-//			this->arr[this->size] = value;
-//			size++;
-//		}
-//		else if (this->size == this->capacity && this->isExpandable == false) cout << "Stack is full " << endl;
-//	}
-//	void pop()
-//	{
-//		if (this->size == 0) cout << "Stack is empty " << endl;
-//		else
-//		{
-//			this->arr[this->size - 1] = NULL;
-//			this->size--;
-//		}
-//	}
-//	T peek()
-//	{
-//		if (this->size == 0) cout << "Stack is empty " << endl;
-//		else return this->arr[this->size - 1];
-//	}
-//	friend ostream& operator<<(ostream& out, const Queue<T>& q)
-//	{
-//		for (int i = 0; i < q.size; i++)
-//		{
-//			out << q.arr[i] << " ";
-//		}
-//		out << endl;
-//		return out;
-//	}
-//};
 int main()
 {
-	Queue<int> arr = {1,2,3,4,5};
+	Queue<int> arr = { 1,2,3,4,5 };
 	arr.pop();
 	arr.push(7);
 	arr.push(5);
 	cout << arr;
 	cout << arr.peek() << endl;
+	DeQueue<int> deArr = { 1,2,3,4,5 };
+	deArr.pushFront(0);
+	deArr.popFront();
+	deArr.pushFront(0);
+	cout << deArr << endl;
+	cout << deArr.peek() << endl;
 	return 0;
 }
