e8d4a76b6db7e32152308c4d240baf358fa4f89c
Bayra_ry03@itstep.edu.az <Bayra_ry03@itstep.edu.az>
2023-12-03 19:50:57 +0400

    HW

---
+++ BookCenter/BookCenter/ViewModels/OrderViewModel.cs
@@ -0,0 +1,44 @@
+﻿using BookCenter.Messages;
+using BookCenter.Models;
+using BookCenter.Services.Classes;
+using BookCenter.Services.Interfaces;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookCenter.ViewModels;
+
+class OrderViewModel : ViewModelBase
+{
+    private readonly INavigationService _navigationService;
+    private readonly IMessenger _messenger;
+    public Result SelectedBook { get; set; }
+
+    public OrderViewModel(INavigationService navigationService, IMessenger messenger)
+    {
+        _navigationService = navigationService;
+        _messenger = messenger;
+
+        _messenger.Register<DataMessage>(this, message =>
+        {
+            if (message.Data != null)
+            {
+                SelectedBook = message.Data as Result;
+            }
+        });
+    }
+    public RelayCommand GoBackCommand
+{
+    get => new(
+        () =>
+        {
+            _navigationService.NavigateTo<InfoViewModel>();
+        }
+    );
+}
---
+++ ToDoListWPF/ToDoListWPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+namespace MyNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddTask(object sender, RoutedEventArgs e)
+        {
+            string newTask = newTaskTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                todoListBox.Items.Add(newTask);
+                newTaskTextBox.Text = "";
+            }
+        }
+        private void RemoveTask(object sender, RoutedEventArgs e)
+        {
+            if (todoListBox.SelectedItem != null)
+            {
+                todoListBox.Items.Remove(todoListBox.SelectedItem);
+            }
+        }
+    }
+}
