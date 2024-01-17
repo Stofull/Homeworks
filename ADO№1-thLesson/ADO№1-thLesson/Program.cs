1580206b41ce2d72dbcf9b299288cf617ec36f19
Stofull <yusif209@gmail.com>
2024-01-17 11:54:46 +0400

    Test

--- ADO№1-thLesson/ADO№1-thLesson/Program.cs
+++ ADO№1-thLesson/ADO№1-thLesson/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+var builder = new ConfigurationBuilder();
+builder.AddJsonFile("appsettings.json");
+var config = builder.Build();
+var connectionString = config.GetConnectionString("Default");
+using SqlConnection conn = new(connectionString);
+conn.Open();
+using SqlCommand command = new("select * from People", conn);
+var reader = command.ExecuteReader();
+while (reader.Read()){
+    var id = reader.GetInt32(0);
+    Console.WriteLine($"{reader[0]}\t{reader[1]}");
+
+}
