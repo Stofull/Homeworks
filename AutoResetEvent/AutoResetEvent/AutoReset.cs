254d1456c3c406c22708c59b8bb0a5b2d68199af
Stofull <yusif209@gmail.com>
2024-06-25 17:25:53 +0400

    System

---
+++ AutoResetEvent/AutoResetEvent/AutoReset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class AutoReset
+{
+    static AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+    static void Main()
+    {
+        Thread workerThread = new Thread(Worker);
+        workerThread.Start();
+
+        Console.WriteLine("Main thread waiting for signal...");
+        autoEvent.WaitOne();
+
+        Console.WriteLine("Main thread received signal.");
+        Console.ReadLine();
+    }
+
+    static void Worker()
+    {
+        Console.WriteLine("Worker thread starting...");
+        Thread.Sleep(2000);
+        Console.WriteLine("Worker thread sending signal.");
+        autoEvent.Set();
+    }
+}
---
+++ AutoResetEvent/AutoResetEvent/ManualReset.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+class ManualReset
+{
+    static ManualResetEvent manualEvent = new ManualResetEvent(false);
+
+    static void Main()
+    {
+        Thread workerThread1 = new Thread(Worker);
+        Thread workerThread2 = new Thread(Worker);
+
+        workerThread1.Start();
+        workerThread2.Start();
+
+        Console.WriteLine("Main thread waiting for signal...");
+        manualEvent.WaitOne();
+
+        Console.WriteLine("Main thread received signal.");
+        manualEvent.Reset();
+        Console.ReadLine();
+    }
+
+    static void Worker()
+    {
+        Console.WriteLine("Worker thread starting...");
+        Thread.Sleep(2000);
+        Console.WriteLine("Worker thread sending signal.");
+        manualEvent.Set();
+    }
+}
