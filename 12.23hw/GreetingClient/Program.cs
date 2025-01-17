0b8ca657d1c88ffcc57e9b5d0bfdc2f6e49890e9
Stofull <yusif209@gmail.com>
2025-01-17 19:15:24 +0400

    HW

---
+++ 12.23hw/GreetingClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GreetingApp;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+        var client = new GreetingService.GreetingServiceClient(channel);
+
+        Console.WriteLine("Enter your name:");
+        var name = Console.ReadLine();
+
+        var request = new HelloRequest { Name = name };
+        var reply = await client.SayHelloAsync(request);
+
+        Console.WriteLine($"Server Response: {reply.Message}");
+    }
+}
